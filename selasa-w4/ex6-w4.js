1142efe7998754b961cd9fae75b9857c0c00b958
Dani Damara <danny12march@gmail.com>
2018-02-14 23:26:45 +0700

    Revisi Exercise 6 week 4

--- selasa-w4/ex6-w4.js
+++ selasa-w4/ex6-w4.js
@@ -8,11 +8,11 @@ karena kata "greatest" memiliki 2 huruf e dan 2 huruf t, dan muncul lebih dulu.
 Jika tidak ditemukan kalimat dengan kata yang memiliki huruf berulang, return -1.
 */
 
+/* Mencari kalimat yang memiliki huruf perulangan yang paling banyak
 function hitungHuruf(kata) {
   var arrKata = kata.split(' ');
   var hasilKata = '';
   var jumlahHuruf = 1;
-  var hurufSama = '';
 
   for(var h = 0; h < arrKata.length; h++){
 
@@ -23,14 +23,18 @@ function hitungHuruf(kata) {
             for(var j = i; j <arrKata[h].length; j++){
                 if(arrKata[h][i] === arrKata[h][j]){
                   counter += 1;
+
                 }
 
                 if(jumlahHuruf < counter){
-                  hurufSama = arrKata[h][i];
+
                   hasilKata = arrKata[h];
                   jumlahHuruf += 1;
+
                 }
+
             }
+
             counter = 0;
       }
 
@@ -40,28 +44,61 @@ function hitungHuruf(kata) {
       return -1;
   }else{
       return hasilKata;
-      //return 'Hasil kalimat : '+hasilKata+'\nHuruf Sama : '+hurufSama+'\nJumlah hurufnya ; '+jumlahHuruf+'\n';
+      //return 'Hasil kalimat : '+hasilKata+'\nHuruf Sama : '+hurufSama+'\nJumlah hurufnya ; '+jumlahHuruf+'\n'+counter2+'\n';
   }
-  /*var hurufSama = '';
-  var jumlahHuruf = 1;
-  var counter = 0;
 
-  for(var i = 0; i < arrKata[3].length; i++){
-      for(var j = i; j <arrKata[3].length; j++){
-          if(arrKata[3][i] === arrKata[3][j]){
-            counter += 1;
-          }
+}
+*/
 
-          if(jumlahHuruf < counter){
-            hurufSama = arrKata[3][i];
-            //hurufSama = arrKata[h];
+//Mencari kalimat yang memilik jumlah huruf sama yang paling banyak dalam 1 kalimat
+function hitungHuruf(kata){
+  var arrKata = kata.split(' ');
+  var hasilKata = '';
+  var jumlahKataSama = 0;
 
-            jumlahHuruf += 1;
-          }
-      }
-      counter = 0;
+  for( var h = 0; h < arrKata.length; h++){
+    var hurufSama = [];
+    var counter = 0;
+
+    for(var i = 0; i < arrKata[h].length; i++){
+        for(var j = i; j < arrKata[h].length; j++){
+            if(arrKata[h][i] === arrKata[h][j]){
+              counter += 1;
+            }
+        }
+
+        if(counter > 1){
+            hurufSama.push(arrKata[h][i]);
+        }
+
+        counter = 0;
+    }
+
+    //Mencari jumlah huruf yang sama dalam 1 kalimat
+    for(var z = 0; z < hurufSama.length; z++){
+        for(var x = 0; x < hurufSama.length; x++){
+            if(z !== x){
+                if(hurufSama[z] === hurufSama[x]){
+                    hurufSama.splice(x, 1);
+                }
+            }
+        }
+    }
+
+    //Cek apabila kalimat sekaranag memiliki jumlah huruf yang lebih besar dari kalimat sebelumnya
+    if(jumlahKataSama < hurufSama.length){
+        jumlahKataSama = hurufSama.length;
+        hasilKata = arrKata[h];
+    }
+
   }
-  return hurufSama+' '+jumlahHuruf;*/
+
+  if(hasilKata !== ''){
+      return hasilKata;
+  }else{
+      return -1;
+  }
+
 }
 
 // TEST CASES
@@ -70,3 +107,7 @@ console.log(hitungHuruf('I am a passionate developer')); // passionate
 console.log(hitungHuruf('aku adalah anak gembala')); // adalah
 console.log(hitungHuruf('rajin pangkal kaya')); // pangkal
 console.log(hitungHuruf('mengayuh perahu di danau')); // danau
+
+//Tes lain
+console.log(hitungHuruf('aabbcc ddddeee'));
+console.log(hitungHuruf('abcdefg'));
